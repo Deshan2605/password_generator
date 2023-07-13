955d8de59da1b4b936a45e48887795773c585523
Deshan2605 <deshanvijayvargiya8888@gmail.com>
2023-07-14 01:12:54 +0530

    Second Commit

--- passwordgenerator.py
+++ passwordgenerator.py
@@ -1,9 +1,24 @@
 import random
-chars="abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()"
-length=int(input("Enter the length of your password: "))
-password=""
 
-for a in range(length):
-    password+=random.choice(chars)
+char1 = "abcdefghijklmnopqrstuvwxyz"
+char2 = "0123456789"
+char3 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+char4 = "!@#$%^&*()"
+char5 = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()"
 
-print("Your password is:",password)
+length = int(input("Enter the length of your password: "))
+password = ""
+
+password += random.choice(char1)  # Ensure the first character is a random lowercase letter
+password += random.choice(char2)
+password += random.choice(char3)
+password += random.choice(char4)
+
+for _ in range(length - 4):
+    password += random.choice(char5)
+
+password_list = list(password)
+random.shuffle(password_list)
+shuffled_password = ''.join(password_list)
+
+print("Your password is:", shuffled_password)
